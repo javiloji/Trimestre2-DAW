ab08eb4633c4b9da6d767e6e541b017661b8c190
javiloji <javierlopera988@gmail.com>
2019-05-23 10:19:05 +0200

    Cambios tabulaciones

--- java/ejercicio2Examen/Cuadrado.java
+++ java/ejercicio2Examen/Cuadrado.java
@@ -2,26 +2,26 @@ package ejercicio2Examen;
 
 /**
  * 
- * Crea la clase Rect�ngulo de forma que:
+ * Crea la clase Rectángulo de forma que:
 
-�Un objeto de esta clase se construye pas�ndole el ancho y el alto. 
-Ninguno de los dos atributospuede ser menor o igual a cero ni mayor que diez, 
-en esos casos se debe lanzar la excepci�n ArithmeticException.
+  Un objeto de esta clase se construye pasándole el ancho y el alto. 
+  Ninguno de los dos atributospuede ser menor o igual a cero ni mayor que diez, 
+  en esos casos se debe lanzar la excepción ArithmeticException.
 
-�Mediante getters y setters permite que se acceda y se modifique el 
-ancho y el alto del rect�ngulo teniendo en cuenta las restricciones en
- cuanto a las dimensiones del apartado anterior.
+  Mediante getters y setters permite que se acceda y se modifique el 
+  ancho y el alto del rectángulo teniendo en cuenta las restricciones en
+  cuanto a las dimensiones del apartado anterior.
  
- �Al imprimir en pantalla un objeto de la clase usando System.out.print se 
- debe dibujar el rect�ngulo por la pantalla (de manera similar a como se imprime 
- un cuadrado en la p�gina 130 del libro Aprende Java con Ejercicios).
+ Al imprimir en pantalla un objeto de la clase usando System.out.print se 
+ debe dibujar el rectángulo por la pantalla (de manera similar a como se imprime 
+ un cuadrado en la página 130 del libro Aprende Java con Ejercicios).
  
- �Crea la clase Cuadrado como subclase de Rect�ngulo. 
- Le debes a�adir a su comportamiento la posibilidad de 
- comparar objetos cuadrados entre s�.
+ Crea la clase Cuadrado como subclase de Rectángulo. 
+ Le debes añadir a su comportamiento la posibilidad de 
+ comparar objetos cuadrados entre sí.
  
- �Crea los programas de test correspondientes a ambas clases. 
- Debes provocar que se lance la excepci�n y capturarla. 
+ Crea los programas de test correspondientes a ambas clases. 
+ Debes provocar que se lance la excepción y capturarla. 
  * 
  * @author Javier Lopera Jimenez
  *
@@ -31,13 +31,13 @@ public class Cuadrado extends Rectangulo implements Comparable<Cuadrado>{
 
   private double lado;
 	
-	public Cuadrado(double lado) {
+  public Cuadrado(double lado) {
 	  
-		super(lado,lado);
-		this.lado=lado;
-	}
+    super(lado,lado);
+    this.lado=lado;
+  }
 	
-	public double getLado() {
+  public double getLado() {
     return lado;
   }
 
@@ -50,8 +50,8 @@ public class Cuadrado extends Rectangulo implements Comparable<Cuadrado>{
       }
   }
 	
-	/**
-	 * 
+  /**
+   * 
 	 * El metodo compararCuadrado al que le pasamos dos cuadrados 
 	 * por parametro y nos indica cual es mayor.
 	 * 
@@ -59,35 +59,36 @@ public class Cuadrado extends Rectangulo implements Comparable<Cuadrado>{
 	 * @param c2
 	 */
 	
-	public void compararCuadrado(Cuadrado c1,Cuadrado c2) {
+  public void compararCuadrado(Cuadrado c1,Cuadrado c2) {
 		
 		
-		if(c1.getLado()>c2.getLado()) {
-			System.out.println("El primer cuadrado es mayor que el segundo cuadrado.");
-		}
-		else if(c1.getLado()==c2.getLado()) {
-			System.out.println("El primer cuadrado es igual que el segundo cuadrado.");
-		}
-		else {
-			System.out.println("El primer cuadrado es menor que el segundo cuadrado.");
-		}
-	}
+    if(c1.getLado()>c2.getLado()) {
+      System.out.println("El primer cuadrado es mayor que el segundo cuadrado.");
+    }
+      else if(c1.getLado()==c2.getLado()) {
+        System.out.println("El primer cuadrado es igual que el segundo cuadrado.");
+      }
+      else {
+        System.out.println("El primer cuadrado es menor que el segundo cuadrado.");
+      }
+  }
 
-	/**
-	 * 
-	 * Creamos el metodo toString para dibujar los cuadrados.
-	 * 
-	 */
+  /**
+   * 
+   * Creamos el metodo toString para dibujar los cuadrados.
+   * 
+   */
 	
-	@Override
-	public String toString() {
+  @Override
+  public String toString() {
 	  
-	  return super.toString();
+    return super.toString();
 		
-	}	
+  }	
 	
-	@Override
-	public int compareTo(Cuadrado cuadrado) {
-	  return (int) (super.getAlto()-cuadrado.getAncho());
-	}
+  @Override
+  public int compareTo(Cuadrado cuadrado) {
+    return (int) (super.getAlto()-cuadrado.getAncho());
+  }
 }
+
--- java/ejercicio2Examen/TestRectangulo.java
+++ java/ejercicio2Examen/TestRectangulo.java
@@ -2,26 +2,26 @@ package ejercicio2Examen;
 
 /**
  * 
- * Crea la clase Rect�ngulo de forma que:
+ * Crea la clase Rectángulo de forma que:
 
-�Un objeto de esta clase se construye pas�ndole el ancho y el alto. 
+Un objeto de esta clase se construye pas�ndole el ancho y el alto. 
 Ninguno de los dos atributospuede ser menor o igual a cero ni mayor que diez, 
-en esos casos se debe lanzar la excepci�n ArithmeticException.
+en esos casos se debe lanzar la excepción ArithmeticException.
 
-�Mediante getters y setters permite que se acceda y se modifique el 
-ancho y el alto del rect�ngulo teniendo en cuenta las restricciones en
+Mediante getters y setters permite que se acceda y se modifique el 
+ancho y el alto del rectángulo teniendo en cuenta las restricciones en
  cuanto a las dimensiones del apartado anterior.
- 
- �Al imprimir en pantalla un objeto de la clase usando System.out.print se 
- debe dibujar el rect�ngulo por la pantalla (de manera similar a como se imprime 
- un cuadrado en la p�gina 130 del libro Aprende Java con Ejercicios).
- 
- �Crea la clase Cuadrado como subclase de Rect�ngulo. 
- Le debes a�adir a su comportamiento la posibilidad de 
- comparar objetos cuadrados entre s�.
- 
- �Crea los programas de test correspondientes a ambas clases. 
- Debes provocar que se lance la excepci�n y capturarla. 
+
+ Al imprimir en pantalla un objeto de la clase usando System.out.print se 
+ debe dibujar el rectángulo por la pantalla (de manera similar a como se imprime 
+ un cuadrado en la página 130 del libro Aprende Java con Ejercicios).
+
+ Crea la clase Cuadrado como subclase de Rectángulo. 
+ Le debes añadir a su comportamiento la posibilidad de 
+ comparar objetos cuadrados entre sí.
+
+ Crea los programas de test correspondientes a ambas clases. 
+ Debes provocar que se lance la excepción y capturarla. 
  * 
  * @author Javier Lopera Jimenez
  *
@@ -29,22 +29,22 @@ ancho y el alto del rect�ngulo teniendo en cuenta las restricciones en
 
 public class TestRectangulo {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		/**
-		 * 
-		 * Capturamos la excepcion y mostramos el mensaje de error en caso de error.
-		 * 
-		 */
-		try {
-		Rectangulo r = new Rectangulo(3,5);
-		System.out.println(r);
-		}catch(ArithmeticException e) {
-			System.err.println("El rectangulo debe tener la base y la altura mayor que cero y menor o igual que 10.");
-		}
-		
-		
-	}
+    /**
+     * 
+     * Capturamos la excepcion y mostramos el mensaje de error en caso de error.
+     * 
+     */
+    try {
+      Rectangulo r = new Rectangulo(3,5);
+      System.out.println(r);
+    }catch(ArithmeticException e) {
+      System.err.println("El rectangulo debe tener la base y la altura mayor que cero y menor o igual que 10.");
+    }
 
-	
+
+  }
+
+
 }
